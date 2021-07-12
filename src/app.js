2eb28ce57255f3b5b7321f4d9107cc89f6912f75
Oluwole <oluwole@Oluwoles-MacBook-Pro.local>
2021-07-12 09:33:04 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+
+const app = express();
+app.use(bodyParser.json()); // support application/json type post data
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+//Enables CORS Request
+app.use(cors());
+//Secure your  app by setting various HTTP headers
+app.use(helmet());
+// Compress response bodies for all requesst that passes through it
+app.use(compression());
+
+module.exports = app;
